414883fb332da8701f7fd2e25d986e436e94b92f
Sting Alleman <stingalleman@icloud.com>
2021-02-15 15:26:43 +0100

    yeet

---
+++ cmd/exec.go
@@ -0,0 +1,43 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/stingalleman/stingshell/util"
+)
+
+// RunCmd execute command.
+func RunCmd(input string) error {
+	input = strings.TrimSuffix(input, "\n")
+	homeDir, _ := os.UserHomeDir()
+
+	input = strings.ReplaceAll(input, "~", homeDir)
+
+	args := strings.Split(input, " ")
+
+	switch args[0] {
+	case "cd":
+		{
+			if len(args) < 2 {
+				homeDir, _ := os.UserHomeDir()
+
+				return os.Chdir(homeDir)
+			}
+			return os.Chdir(args[1])
+		}
+	case "exit":
+		{
+			fmt.Fprint(os.Stderr, "bye bye!\n")
+			util.ExitShell()
+		}
+	}
+	cmd := exec.Command(args[0], args[1:]...)
+
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	return cmd.Run()
+}
